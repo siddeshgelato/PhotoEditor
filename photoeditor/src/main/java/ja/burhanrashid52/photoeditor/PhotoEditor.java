455dc97f42b9ed1d70ff02b58ac25d96e631ab4e
siddesh <siddesh.gandhe@gelato.com>
2021-06-08 17:46:36 +0530

    -Height width issue

--- photoeditor/src/main/java/ja/burhanrashid52/photoeditor/PhotoEditor.java
+++ photoeditor/src/main/java/ja/burhanrashid52/photoeditor/PhotoEditor.java
@@ -213,7 +213,7 @@ public class PhotoEditor implements BrushViewChangeListener {
         final FrameLayout frmBorder = textRootView.findViewById(R.id.frmBorder);
 
 
-        FrameLayout.LayoutParams params = (FrameLayout.LayoutParams) imageView.getLayoutParams();
+        FrameLayout.LayoutParams params = (FrameLayout.LayoutParams) textRootView.getLayoutParams();
 
         if (height > 0.0 && width > 0.0) {
             params.height = (int) height;
