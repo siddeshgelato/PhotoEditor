ad3192438af8103387208780ef13f8f4d3b25cdc
siddeshgandhe <siddesh.gandhe@gelato.com>
2022-03-14 10:00:58 +0530

    - Changes for external newly designed undo redo

--- photoeditor/src/main/java/ja/burhanrashid52/photoeditor/OnElementSelectionListener.java
+++ photoeditor/src/main/java/ja/burhanrashid52/photoeditor/OnElementSelectionListener.java
@@ -3,5 +3,5 @@ package ja.burhanrashid52.photoeditor;
 import android.view.View;
 
 public interface OnElementSelectionListener {
-    void onElementSelectedDeselected(View view, boolean isSelected);
+    void onElementSelectedDeselected(View view, boolean isSelected, boolean isUserClicked);
 }
